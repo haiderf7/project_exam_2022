2b95dd4439acd9a64135e1963e2cdefd77b65386
haiderf7 <91637854+haiderf7@users.noreply.github.com>
2022-11-02 17:40:51 +0100

    new

--- js/blog.specific.js
+++ js/blog.specific.js
@@ -5,39 +5,46 @@ const id = params.get("id");
 console.log(id);
 console.log(params);
 
-const blogsUrl =
-  "https://haiderhashem.one/project-exam-1/wp-json/wp/v2/posts" + id;
+const cors = "https://noroffcors.herokuapp.com/";
+const url =
+"https://haiderhashem.one/project-exam-1/wp-json/wp/v2/posts"+ id;
 
+const corsUrl = cors + url;
 
+console.log(corsUrl);
 
-  const corsUrl = url;
-
-console.log(url);
-
-const locationsContainer = document.querySelector(".locationsContainer");
+const productContainer = document.querySelector(".api-blog");
 
 function error(message = "error") {
-	return `<div class"error">${message}</div>`;
+  return `<div class"error">${message}</div>`;
 }
 
-// console.log(corsUrl);
+console.log(corsUrl);
 
-async function getProductDetail() {
-	try {
-		const response = await fetch(corsUrl);
-		const results = await response.json();
-		const product = results;
+async function apiBlogs() {
+  try {
+    const response = await fetch(corsUrl);
+    const results = await response.json();
+    const product = results;
+    document.title = results.title.rendered;
 
-		console.log(product);
-
-		locationsContainer.innerHTML += `
-                                    <div class="details">
-                                        <h2>${product.name}</h2>
-                                        <img src="${product.images[0].src}" />
-                                    </div>`;
-	} catch (error) {
-		console.log(error);
-	}
+    console.log(product);
+    productContainer.innerHTML += `
+        <div class="details">
+        <div><h2>${results.title.rendered}</h2></div>
+        <div><img onclick="onClick(this)" class="modalHoverOpacity"
+        <div><img src="${results[i].jetpack_featured_media_url}" alt="Picture of ${results[i].title.rendered}"></img></div>
+        <div>${results.excerpt.rendered}</div>
+        <div>${results.content.rendered}</div>
+      </div>`;
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-getProductDetail();
+apiBlogs();
+
+function onClick(element) {
+  document.getElementById("imageToId").src = element.src;
+  document.getElementById("modalToId").style.display = "block";
+}
